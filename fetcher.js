a8359804091131cf67805704cfa7621e3dc24962
Ryan Kwok <kaj.kwok@gmail.com>
2022-03-16 23:31:07 -0700

    added prompt to overwrite file if exists

--- fetcher.js
+++ fetcher.js
@@ -1,19 +1,39 @@
 const request = require('request');
 const fs = require('fs')
+const readline = require('readline');
 const args = process.argv.slice(2);
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 request(args[0], (error, response, body) => {
   if(error) {
     console.log("there was an error")
     return
   }
-  if(response !== 200){
+  if(response.statusCode !== 200){
     console.log("bad page")
     return
   }
-  fs.writeFile(args[1], body, error => {
+    fs.writeFile(args[1], body, { flag: "wx" }, error => {
       if (error) {
-        console.log('failed download')
+        rl.question("file exists, overwrite?", (answer) =>{
+          if(answer === 'y'){
+            fs.writeFile(args[1], body, error => {
+              if(error){
+                console.log("failed")
+              }
+              size = 0
+              stats = fs.stat(args[1], (error, stats) => {
+              size = stats.size
+              console.log(`downloaded and saved to ${args[1]}, size ${size} kB`)
+              rl.close()
+            })
+          })
+        }
+        })
         return
       }
       size = 0
@@ -23,12 +43,3 @@ request(args[0], (error, response, body) => {
       })
     })
 });
-
-  // console.log("starting download")
-  // fs.writeFile(args[1], data, error => {
-  //   if (error) {
-  //     console.log('failed download')
-  //     return
-  //   }
-  //   console.log(`downloaded and saved to ${args[1]}`)
-  // })
